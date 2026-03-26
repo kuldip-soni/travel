4b372b8e4f39e2ec6b9fbab51aae47fa05478d11
220490131088kuldip <220490131088kuldip1234@gmail.com>
2026-03-26 15:14:27 +0530

    login & logout

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,21 +2,24 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import{BrowserRouter} from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import store  from './redux/store.js'
+import store, { persistor } from './redux/store.js'
+import { PersistGate } from 'redux-persist/integration/react'
 
 
 createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-  <BrowserRouter>
-  <StrictMode>
-    <App />
-  </StrictMode>,
-  </BrowserRouter>
-   </Provider>
+  <PersistGate loading={null} persistor={persistor}>
 
-  
-  
-    
+    <Provider store={store}>
+      <BrowserRouter>
+        <StrictMode>
+          <App />
+        </StrictMode>,
+      </BrowserRouter>
+    </Provider>
+
+  </PersistGate>
+
+
 )
--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -42,43 +42,43 @@
 // // Posted by Manuj Kathuria
 // // Retrieved 2026-03-26, License - CC BY-SA 4.0
 
-import {configureStore} from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import storage from 'redux-persist/lib/storage'
-import {combineReducers} from "redux"; 
-import { persistReducer } from 'redux-persist'
-import {thunk} from 'redux-thunk'
-import locationSlice  from './slice/location.slice'
-import  packageSlice    from './slice/package.slice'
-import  itinearySlice  from './slice/itineary.slice'
-import  blogSlice  from './slice/blog.slice'
-import  transportSlice  from './slice/transport.slice'
-import  vendorSlice  from './slice/vendor.slice'
-import  serviceSlice  from './slice/service.slice'
-import  roomSlice  from './slice/room.slice'
+import { combineReducers } from "redux";
+import { persistReducer, persistStore } from 'redux-persist'
+import { thunk } from 'redux-thunk'
+import locationSlice from './slice/location.slice'
+import packageSlice from './slice/package.slice'
+import itinearySlice from './slice/itineary.slice'
+import blogSlice from './slice/blog.slice'
+import transportSlice from './slice/transport.slice'
+import vendorSlice from './slice/vendor.slice'
+import serviceSlice from './slice/service.slice'
+import roomSlice from './slice/room.slice'
 import hotelSlice from './slice/hotel.slice'
-import restaurantSlice  from './slice/restaurant.slice'
-import contectSlice  from './slice/contect.slice'
+import restaurantSlice from './slice/restaurant.slice'
+import contectSlice from './slice/contect.slice'
 import authSlice from './slice/auth.slice'
 
 const reducers = combineReducers({
- location: locationSlice,
-    package: packageSlice,
-    itineary:itinearySlice,
-    blog:blogSlice,
-    transport:transportSlice,
-    vendor:vendorSlice,
-    service:serviceSlice,
-    room:roomSlice,
-    hotel:hotelSlice,
-    restaurant:restaurantSlice,
-    contect:contectSlice,
-    auth: authSlice           
+  location: locationSlice,
+  package: packageSlice,
+  itineary: itinearySlice,
+  blog: blogSlice,
+  transport: transportSlice,
+  vendor: vendorSlice,
+  service: serviceSlice,
+  room: roomSlice,
+  hotel: hotelSlice,
+  restaurant: restaurantSlice,
+  contect: contectSlice,
+  auth: authSlice
 });
- 
+
 const persistConfig = {
-    key: 'root',
-    storage,
-    whitelist: ['auth']
+  key: 'root',
+  storage,
+  whitelist: ['auth']
 };
 
 const persistedReducer = persistReducer(persistConfig, reducers);
@@ -86,9 +86,11 @@ const persistedReducer = persistReducer(persistConfig, reducers);
 
 const store = configureStore(
   {
-    reducer: persistedReducer, 
-    // middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(auth.middleware),
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
   });
+
+export let persistor = persistStore(store)
 
 
 export default store;
