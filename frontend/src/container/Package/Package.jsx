42147be5be2cd33906dd0899193ffecf1c1945c7
220490131088kuldip <220490131088kuldip1234@gmail.com>
2026-04-06 21:41:51 +0530

    oldwork

--- frontend/src/container/Package/Package.jsx
+++ frontend/src/container/Package/Package.jsx
@@ -17,10 +17,10 @@ function Package(props) {
         <div>
             <section id="Popular-Packages">
                 <div className="container">
-                    <h2 className="main-title">Packages</h2>
-                    <p className="sub-title">
+                    {/* <h2 className="main-title">Packages</h2> */}
+                    {/* <p className="sub-title">
                         Our travel management system offers a variety of travel packages designed to meet the needs of different types of users. These packages include arrangements for transportation, accommodation, and sightseeing activities. The main objective is to provide a complete travel solution in one place.
-                    </p>
+                    </p> */}
                     <div className="row all-popular">
                         <div className="Popular-data col-lg-6">
                             <h2>Popular Packages</h2>
